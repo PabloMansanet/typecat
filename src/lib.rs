367f8b88ca378a16c65a6d46f065a6fedc55654c
PabloMansanet <pablomansanet@gmail.com>
2020-06-02 12:53:47 +0200

    Add upper and lower concatenate

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,23 @@ pub fn concatenate(_item: TokenStream) -> TokenStream {
         .collect();
     text.parse().unwrap()
 }
+
+#[proc_macro]
+pub fn concatenate_upper(_item: TokenStream) -> TokenStream {
+    let text: String = _item
+        .into_iter()
+        .filter_map(|t| if let TokenTree::Ident(i) = t { Some(i.to_string()) } else { None })
+        .collect::<String>()
+        .to_uppercase();
+    text.parse().unwrap()
+}
+
+#[proc_macro]
+pub fn concatenate_lower(_item: TokenStream) -> TokenStream {
+    let text: String = _item
+        .into_iter()
+        .filter_map(|t| if let TokenTree::Ident(i) = t { Some(i.to_string()) } else { None })
+        .collect::<String>()
+        .to_lowercase();
+    text.parse().unwrap()
+}
